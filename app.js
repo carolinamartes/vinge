e35af1ffaf7c5014103378382b97173de9a84341
Carolina Martes <carolina.martes@gmail.com>
2016-08-12 18:05:00 -0400

    Add binging capabilities

--- app.js
+++ app.js
@@ -16,12 +16,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 // app.use(countingVideos())
+var type="";
 
-app.get('/search/:query', function(req, res) {
+
+app.get('/search/:query/:counter', function(req, res) {
   var query = req.params.query;
-
-  var url = 'https://www.tastekid.com/api/similar?q=' + query + '&verbose=1&k=' + process.env.pass
-  var videoCounter=0;
+  var url = 'https://www.tastekid.com/api/similar?q=' + query + "&type=" + type+'&verbose=1&k=' + process.env.pass
+  var videoCounter= req.params.counter;
+  console.log(videoCounter)
   request(url, function(error, response, data) {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(data);
@@ -30,6 +32,7 @@ app.get('/search/:query', function(req, res) {
     }
   })
 })
+
 
 
 app.use(session({
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,43 @@
+'use strict';
 $(document).ready(function() {
 
 $('select').material_select();
 $(".dropdown-button").dropdown();
 
+var counter=0;
+var query="";
+
+$('.searchBar').on('submit', function (e){
+e.preventDefault();
+query= $('input').val();
+
+window.location="/search/"+query+"/"+counter
+})
 
 
-// $("body").keydown(function(e) {
-//   if(e.keyCode == 37) {
-//
-//   }
+
+$("body").keydown(function (e) {
+  if(e.keyCode == 39) {
+
+query = window.location.pathname.replace('/search/','')
+query = query.replace(/\/[^\/]*$/,'')
+
+
+counter= window.location.pathname.replace(/(...+\/)/, '');
+counter++
+
+window.location="/search/"+query+"/"+counter
+}
+
+$("#dropdownMovies").on("click", function(){
+type=$(this).val()
+window.location="/search/"+query+"/"+counter
+})
+
+
+
+
+  });
+
+
+ });
