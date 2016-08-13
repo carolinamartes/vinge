afbf5fb4f3d174a0729e4472d7ac66cd78d5585c
Carolina Martes <carolina.martes@gmail.com>
2016-08-13 18:29:21 -0400

    Add semi-ok autocomplete

--- app.js
+++ app.js
@@ -16,13 +16,24 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-
+app.get('/autocomplete/:input', function(req,res){
+  var input= req.params.input;
+  var url= "http://www.omdbapi.com/?t=" + input + "&r=json";
+  request(url, function(error, response, data) {
+    if (!error && response.statusCode == 200) {
+    var movie_data = JSON.parse(data);
+    var title = movie_data.Title;
+    console.log(title)
+    res.send(title)
+    }
+  })
+})
 
 app.get('/search/:query/:Qtype/:counter/', function(req, res) {
   var query = req.params.query;
-  var type= req.params.Qtype || "";
-  var url = 'https://www.tastekid.com/api/similar?q=' + query +  "&type=" + type+'&verbose=1&k=' + process.env.pass
-  var videoCounter= req.params.counter;
+  var type = req.params.Qtype || "";
+  var url = 'https://www.tastekid.com/api/similar?q=' + query + "&type=" + type + '&verbose=1&k=' + process.env.pass
+  var videoCounter = req.params.counter;
   console.log(videoCounter)
   request(url, function(error, response, data) {
     if (!error && response.statusCode == 200) {
@@ -32,7 +43,6 @@ app.get('/search/:query/:Qtype/:counter/', function(req, res) {
     }
   })
 })
-
 
 app.use(session({
   secret: 'demo-secret',
--- public/js/script.js
+++ public/js/script.js
@@ -1,52 +1,69 @@
 'use strict';
 $(document).ready(function() {
 
-$('select').material_select();
-$(".dropdown-button").dropdown();
+  $('select').material_select();
+  $(".dropdown-button").dropdown();
 
-var counter=0;
-var query="";
-
-$('.searchBar').on('submit', function (e){
-e.preventDefault();
-query= $('input').val();
-
-  window.location="/search/"+query+"/all/"+counter
+$("input.autocomplete").on('input', function() {
+  var input = $('input').val();
+$.ajax({
+"method": "get",
+"url": "/autocomplete/"+input,
+"success" : function(title){
+  var data={};
+  var key=title
+  data[key]=null;
+  console.log(data)
+  $("input.autocomplete").autocomplete({
+  data
 })
-
-
-$("body").keydown(function (e) {
-  if(e.keyCode == 39) {
-
-    var myString = window.location.pathname;
-    var myRegexp = /\/search\/(.*)\/\d/;
-    var match = myRegexp.exec(myString);
-    var query=match[1]
-
-counter= window.location.pathname.replace(/(...+\/)/, '');
-counter++
-
-  window.location="/search/"+query+"/all/"+counter
+},
+"error": function(){
+  console.log("error")
 }
+})
+});
 
+  var counter = 0;
+  var query = "";
+
+  $('.searchBar').on('submit', function(e) {
+    e.preventDefault();
+    query = $('input').val();
+
+    window.location = "/search/" + query + "/all/" + counter
+  })
+
+
+  $("body").keydown(function(e) {
+    if (e.keyCode == 39) {
+
+      var myString = window.location.pathname;
+      var myRegexp = /\/search\/(.*)\/\d/;
+      var match = myRegexp.exec(myString);
+      var query = match[1]
+
+      counter = window.location.pathname.replace(/(...+\/)/, '');
+      counter++
+
+      window.location = "/search/" + query + "/all/" + counter
+    }
   });
 
+  $(".mediaOptions").on("click", function() {
 
-$(".mediaOptions").on("click", function(){
-//get correct regexs
-
-var myString = window.location.pathname;
-var myRegexp = /(search)\/(.*)\/(all|music|movies|shows)/
-var match = myRegexp.exec(myString);
-var matcharr=match[0].split('/');
-query= matcharr[1]
-Qtype= matcharr[2]
+    var myString = window.location.pathname;
+    var myRegexp = /(search)\/(.*)\/(all|music|movies|shows)/
+    var match = myRegexp.exec(myString);
+    var matcharr = match[0].split('/');
+    query = matcharr[1]
+    Qtype = matcharr[2]
 
 
-  var Qtype=$(this).attr('id');
-  window.location="/search/"+query+"/"+Qtype+"/"+counter
+    var Qtype = $(this).attr('id');
+    window.location = "/search/" + query + "/" + Qtype + "/" + counter
 
   })
 
 
- });
+});
