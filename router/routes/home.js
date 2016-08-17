b10494cfbb6cffee9a4deb64c6caea69fa192bc4
Carolina Martes <carolina.martes@gmail.com>
2016-08-17 02:05:26 -0400

    Fix db problem

--- router/routes/home.js
+++ router/routes/home.js
@@ -3,7 +3,7 @@ const urlgrey= require('urlgrey');
 const router = express.Router();
 const querystring = require('querystring');
 const pgp = require('pg-promise')();
-const db = pgp('postgres://carolinamartes@localhost:5432/auth_p2');
+const db = pgp('postgres://lybtopytzzzqbf:wUHMkDGqj2-oTUith7hiqN5PM8@ec2-54-243-48-181.compute-1.amazonaws.com:5432/dam43ogo8qjd53');
 
 router.get('/', function (req, res){
   if(!req.session.user){
