f6eb949e9f692ed1e9823f1e16a6e86e95ebb850
Carolina Martes <carolina.martes@gmail.com>
2016-08-17 03:05:57 -0400

    W

--- public/js/script.js
+++ public/js/script.js
@@ -79,6 +79,7 @@ $(document).ready(function() {
   $("#autocomplete-input").on('keypress', function() {
     $('#welcome').css("display", "none")
     $('#welcome').css("opacity", 0)
+    $('#welcome').text()="";
     var input = $('input').val();
     $.ajax({
       "method": "get",
